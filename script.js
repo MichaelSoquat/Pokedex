0639c72862c2457958a3c1677ec1d38fb8b9507f
MichaelSoquat <michael.soquat@gmx.de>
2022-01-11 12:07:26 +0100

    added bg-colors

--- script.js
+++ script.js
@@ -36,13 +36,60 @@ function renderPokemonInfo() {
     document.getElementById('pokedex').innerHTML += `<div id="card${currentPokemon.order}" class="pokemonContainer"><span class="styleOrder">${`#` + order}</span><span class="centerName">${currentPokemon['species']['name'][0].toUpperCase()+currentPokemon['species']['name'].substring(1)}</span>
     <img class="pics" src="${currentPokemon.sprites.front_shiny}"><div id="spaceBetweenType" class="typeSpaceBetween"><div class="styleType">${currentPokemon.types[0].type.name[0].toUpperCase()+currentPokemon.types[0].type.name.substring(1)}</div><div id="${currentPokemon.order}" class="d-none styleType">${type2}</div></div></div>`;
 
-    if (type2) {
-        document.getElementById(`${currentPokemon.order}`).classList.remove(`d-none`);
-    }
-
-    if (currentPokemon.types[0].type.name[0].toUpperCase()+currentPokemon.types[0].type.name.substring(1) == 'Grass') {
-        document.getElementById(`card${currentPokemon.order}`).classList.add('bgGreen');
-    }
+    checkIfSecondType(type2);
+    backgroundColor();
+    
 }
 
 
+function checkIfSecondType(type2) {
+    if (type2) {
+        document.getElementById(`${currentPokemon.order}`).classList.remove(`d-none`);
+    }
+}
+
+function backgroundColor() {
+    if (currentPokemon.types[0].type.name === 'grass'||'bug'||'poison') {
+        document.getElementById(`card${currentPokemon.order}`).classList.add('bgGreen');
+    }
+
+    if (currentPokemon.types[0].type.name === 'fire') {
+        document.getElementById(`card${currentPokemon.order}`).classList.add('bgRed');
+    }
+
+    if (currentPokemon.types[0].type.name[0].toUpperCase()+currentPokemon.types[0].type.name.substring(1) === 'Water') {
+        document.getElementById(`card${currentPokemon.order}`).classList.add('bgBlue');
+    }
+
+    if (currentPokemon.types[0].type.name[0].toUpperCase()+currentPokemon.types[0].type.name.substring(1) === 'Electric') {
+        document.getElementById(`card${currentPokemon.order}`).classList.add('bgYellow');
+    }
+
+    if (currentPokemon.types[0].type.name[0].toUpperCase()+currentPokemon.types[0].type.name.substring(1) === 'Normal') {
+        document.getElementById(`card${currentPokemon.order}`).classList.add('bgGrey');
+    }
+
+    if (currentPokemon.types[0].type.name[0].toUpperCase()+currentPokemon.types[0].type.name.substring(1) === 'Fairy') {
+        document.getElementById(`card${currentPokemon.order}`).classList.add('bgGrey');
+    }
+
+    if (currentPokemon.types[0].type.name[0].toUpperCase()+currentPokemon.types[0].type.name.substring(1) === 'Ground') {
+        document.getElementById(`card${currentPokemon.order}`).classList.add('bgBrown');
+    }
+
+    if (currentPokemon.types[0].type.name[0].toUpperCase()+currentPokemon.types[0].type.name.substring(1) === 'Fighting') {
+        document.getElementById(`card${currentPokemon.order}`).classList.add('bgBrown');
+    }
+
+    if (currentPokemon.types[0].type.name[0].toUpperCase()+currentPokemon.types[0].type.name.substring(1) === 'Rock') {
+        document.getElementById(`card${currentPokemon.order}`).classList.add('bgBrown');
+    }
+
+    if (currentPokemon.types[0].type.name[0].toUpperCase()+currentPokemon.types[0].type.name.substring(1) === 'Psychic') {
+        document.getElementById(`card${currentPokemon.order}`).classList.add('bgPurple');
+    }
+
+    if (currentPokemon.types[0].type.name[0].toUpperCase()+currentPokemon.types[0].type.name.substring(1) === 'Ghost') {
+        document.getElementById(`card${currentPokemon.order}`).classList.add('bgPurple');
+    }
+}
